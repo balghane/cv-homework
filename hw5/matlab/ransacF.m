09ad7b49f5ed4a3663eb0fcc6fd69e9ac30387a5
basel <basel@umich.edu>
2018-04-09 20:01:33 -0400

    ransac adjustments

--- hw5/matlab/ransacF.m
+++ hw5/matlab/ransacF.m
@@ -1,4 +1,4 @@
-function [ F ] = ransacF( pts1, pts2, M )
+function [ F, inliers ] = ransacF( pts1, pts2, M )
 % ransacF:
 %   pts1 - Nx2 matrix of (x,y) coordinates
 %   pts2 - Nx2 matrix of (x,y) coordinates
@@ -20,11 +20,11 @@ pts2_padded = [pts2 ones(n_pts, 1)];
 best_F = zeros(3, 3);
 best_n_inliers = 0;
 best_inliers = zeros(n_pts, 1);
-n_its = 10000;
-epsilon = 0.001;
+n_its = 20000;
+epsilon = 0.005;
 
 for i = 1:n_its
-    inds = randperm(n_pts,8);
+    inds = randperm(n_pts,100);
     cur_pts1 = pts1(inds, :);
     cur_pts2 = pts2(inds, :);
     F = eightpoint( cur_pts1, cur_pts2, M );
@@ -33,7 +33,7 @@ for i = 1:n_its
     inliers = zeros(n_pts, 1);
     
     for j = 1:n_pts
-        err = pts2_padded(j, :) * F * pts1_padded(j, :)';
+        err = pts2_padded(j, :) * F' * pts1_padded(j, :)';
         if abs(err) < epsilon
             n_inliers = n_inliers + 1;
             inliers(j) = 1;
@@ -44,12 +44,19 @@ for i = 1:n_its
         best_F = F;
         best_n_inliers = n_inliers;
         best_inliers = inliers;
+        
+        %disp('errs')
+        %for k = 1:12
+        %    ind = inds(k);
+        %    pts2_padded(ind, :) * F' * pts1_padded(ind, :)'
+        %end
     end
     
 end
 
 best_n_inliers / n_pts
-F = best_F;
+inliers = best_inliers;
+F = best_F
 
 end
 
