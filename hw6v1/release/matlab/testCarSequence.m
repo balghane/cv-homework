37564596966af8ce21862e8d2051af6f00dc398b
basel <basel@umich.edu>
2018-04-22 20:39:01 -0400

    working 1.2

--- hw6v1/release/matlab/testCarSequence.m
+++ hw6v1/release/matlab/testCarSequence.m
@@ -5,19 +5,23 @@ load('../data/carseq.mat')
 rect = [60 117 146 152]';
 rects = zeros(nf, 4);
 rects(1, :) = rect';
-scale_size = 4;
+scale_size = 2;
+every_x = 1;
 
-for i=1:nf-1
-    f1 = frames(:, :, i);
-    f2 = frames(:, :, i+1);
-    [u,v] = LucasKanadeInverseCompositional(f1, f2, rect);
-    rect = [v+rect(1) ; u+rect(2) ; v+rect(3) ; u+rect(4)];
+for i=1:every_x:nf-1
+    f1 = double(frames(:, :, i));
+    f2 = double(frames(:, :, i+every_x));
+    [u,v] = LucasKanadeInverseCompositional(f1, f2, rects(i, :));
+    rect = [-u+rect(1) ; -v+rect(2) ; -u+rect(3) ; -v+rect(4)];
     rect = round(rect);
-    rects(i+1, :) = rect';
-    if mod(i, 5) == 0
-        i
-        rect
-        imshow(imresize(f2(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+    rects(i+every_x, :) = rect';
+    i
+    if mod(i, 100) == 1
+        % rect
+        % imshow(imresize(f2(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+        imshow(imresize(rescale(f2), scale_size, 'nearest'));
+        rect_s = rect * scale_size;
+        rectangle('Position',[rect_s(1) rect_s(2) rect_s(3)-rect_s(1) rect_s(4)-rect_s(2)],'EdgeColor','y');
         waitforbuttonpress
     end
 end
--- hw6v1/release/matlab/testUltrasoundSequence.m
+++ hw6v1/release/matlab/testUltrasoundSequence.m
@@ -1,1 +1,28 @@
-% your code here
+load('../data/usseq.mat')
+
+[~, ~, nf] = size(frames);
+
+rect = [60 117 146 152]';
+rects = zeros(nf, 4);
+rects(1, :) = rect';
+scale_size = 2;
+every_x = 1;
+
+for i=1:every_x:nf-1
+    f1 = double(frames(:, :, i));
+    f2 = double(frames(:, :, i+every_x));
+    [u,v] = LucasKanadeInverseCompositional(f1, f2, rects(i, :));
+    rect = [-u+rect(1) ; -v+rect(2) ; -u+rect(3) ; -v+rect(4)];
+    rect = round(rect);
+    rects(i+every_x, :) = rect';
+    i
+    if mod(i, 100) == 1
+        % rect
+        % imshow(imresize(f2(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+        imshow(imresize(rescale(f2), scale_size, 'nearest'));
+        rect_s = rect * scale_size;
+        rectangle('Position',[rect_s(1) rect_s(2) rect_s(3)-rect_s(1) rect_s(4)-rect_s(2)],'EdgeColor','y');
+        waitforbuttonpress
+    end
+end
+
