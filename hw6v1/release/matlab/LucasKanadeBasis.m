17961086a5e081c9fd49dc50057b618440f7bb4b
basel <basel@umich.edu>
2018-04-24 14:47:27 -0400

    Part 2 kinda working

--- hw6v1/release/matlab/LucasKanadeBasis.m
+++ hw6v1/release/matlab/LucasKanadeBasis.m
@@ -2,4 +2,78 @@ function [u,v] = LucasKanadeBasis(It, It1, rect, bases)
 
 % input - image at time t, image at t+1, rectangle (top left, bot right
 % coordinates), bases 
-% output - movement vector, [u,v] in the x- and y-directions.
+% output - movement vector, [u,v] in the x- and y-directions.s
+
+% vectorize base
+[bh, bw, bnf] = size(bases);
+bases_vec = reshape(bases, bh*bw, bnf);
+
+% get gradients of bases
+base_grads_x = zeros(bh * bw, bnf);
+base_grads_y = zeros(bh * bw, bnf);
+for i = 1:bnf
+    [tempo_x,tempo_y] = imgradientxy(bases(:, :, i));
+    base_grads_x(:, i) = tempo_x(:);
+    base_grads_y(:, i) = tempo_y(:);
+end
+
+% make template
+template = It( rect(2):rect(4), rect(1):rect(3));
+template_vec = template(:);
+
+% compute image gradient
+[I_x_temp,I_y_temp] = imgradientxy(template);
+I_x_temp = I_x_temp(:);
+I_y_temp = I_y_temp(:);
+
+epsilon = .05;
+vars = zeros(2+bnf, 1);
+delta_vars = ones(2+bnf, 1);
+lambdas = vars(3:2+bnf);
+warp_33 = [1 0 vars(1) ; 0 1 vars(2) ; 0 0 1]';
+warp_form = affine2d(warp_33);
+Rin = imref2d(size(It));
+
+while(norm(delta_vars(1:2)) > epsilon)
+    It1_warp = imwarp(It1, warp_form, 'Linear', 'OutputView', Rin);
+    It1_warp_wind = It1_warp(rect(2):rect(4), rect(1):rect(3));
+    It1_warp_vec = It1_warp_wind(:);
+    
+    % compute error image
+    err_im = template_vec + bases_vec * lambdas - It1_warp_vec;
+    % err_im_temp = template - It1_warp( rect(2):rect(4), rect(1):rect(3));
+    % err_im_temp = err_im_temp(:);
+    
+    % compute steepest descent
+    s_d = [I_x_temp+base_grads_x*lambdas  I_y_temp+base_grads_y*lambdas  bases_vec];
+    
+    % evaluate Hessian
+    H = s_d' * s_d;
+    
+    temp_var = s_d' * err_im;
+    
+    delta_vars = - (H \ temp_var);
+    vars(1:2) = vars(1:2) - delta_vars(1:2);
+    vars(3:2+bnf) = vars(3:2+bnf) - delta_vars(3:2+bnf);
+    lambdas = vars(3:2+bnf);
+    warp_33 = [1 0 vars(1) ; 0 1 vars(2) ; 0 0 1]';
+    warp_form = affine2d(warp_33);
+    % disp('Press a key !')  % Press a key here.You can see the message 'Paused: Press any key' in        % the lower left corner of MATLAB window.
+    % pause;
+end
+
+u = vars(1);
+v = vars(2);
+
+[u v]
+
+% scale_size = 4;
+
+% figure, imshow(imresize(It(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+% figure, imshow(imresize(It1(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+% figure, imshow(imresize(It1_warp(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+% figure, imshow(err_im)
+
+% figure, imshow(It)
+% figure, imshow(It1)
+% figure, imshow(It1_warp)
--- hw6v1/release/matlab/testSylvSequence.m
+++ hw6v1/release/matlab/testSylvSequence.m
@@ -1,1 +1,29 @@
-% your code here
+load('../data/sylvbases.mat')
+load('../data/sylvseq.mat')
+
+
+[~, ~, nf] = size(frames);
+rect = [102 62 156 108]';
+rects = zeros(nf, 4);
+rects(1, :) = rect';
+scale_size = 2;
+every_x = 1;
+
+for i=1:every_x:nf-1
+%for i=1:every_x:5
+    f1 = double(frames(:, :, i));
+    f2 = double(frames(:, :, i+every_x));
+    [u,v] = LucasKanadeBasis(f1, f2, rects(i, :), bases);
+    rect = [u+rect(1) ; v+rect(2) ; u+rect(3) ; v+rect(4)];
+    rect = round(rect);
+    rects(i+every_x, :) = rect';
+    i
+    if mod(i, 100) == 5
+        % rect
+        % imshow(imresize(f2(rect(2):rect(4), rect(1):rect(3)), scale_size, 'nearest'));
+        imshow(imresize(rescale(f2), scale_size, 'nearest'));
+        rect_s = rect * scale_size;
+        rectangle('Position',[rect_s(1) rect_s(2) rect_s(3)-rect_s(1) rect_s(4)-rect_s(2)],'EdgeColor','y');
+        waitforbuttonpress
+    end
+end
